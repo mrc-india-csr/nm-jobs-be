0ca783765790c9e24d50c3bd70f7fcd8f52b0551
Achuthan S <AchuthanSivakumar.S@mrcooper.com>
2023-02-17 11:27:56 +0530

    company serailizer

--- naan_muthalvan/nm_jobs/urls.py
+++ naan_muthalvan/nm_jobs/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = "index"),
+    path('companies/', views.CompanyList.as_view(), name = "company"),
 ]
--- naan_muthalvan/nm_jobs/views.py
+++ naan_muthalvan/nm_jobs/views.py
@@ -1,10 +1,24 @@
-from django.http import HttpResponse, JsonResponse
-from .models import Jobs
+# from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+from .serailizers import CompanySerializers
+# from django.views import View
+# from django.shortcuts import render
+from rest_framework.views import APIView
 
-def index(request):
-    print(request)
-    print(request.method)
-    print(Jobs.objects.first())
-    data = Jobs.objects.first()
-    print(data.description)
-    return JsonResponse({"data": "some data"})
+from .models import Company
+
+# @api_view(['GET'])
+# def company(request):
+#     companies = Company.objects.all()
+#     serializer = CompanySerializers(companies, many = True)
+#     return Response(serializer.data)
+
+class CompanyList(APIView):
+    def get(self, request):
+        companies = Company.objects.all()
+        serializer = CompanySerializers(companies, many = True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        pass
