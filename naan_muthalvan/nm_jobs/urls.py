e67b36d74b0fe61176fcfc939c1d5c138001cdb1
Achuthan S <AchuthanSivakumar.S@mrcooper.com>
2023-02-26 12:30:33 +0530

    url slash handled

--- naan_muthalvan/nm_jobs/urls.py
+++ naan_muthalvan/nm_jobs/urls.py
@@ -1,14 +1,15 @@
-from django.urls import path
-from nm_jobs.views import PerksView, CompanyList, SpocView, CompanyWithName, CompanyView, index, PostJob
+from django.urls import re_path
+from nm_jobs.views import PerksView, CompanyList, SpocView, CompanyWithName, CompanyView, CreateProfile, health_response, post_job
 
 from . import views
 
 urlpatterns = [
-    path('companies', CompanyList.as_view(), name = "company"),
-    path('companies/<str:name>', CompanyWithName.as_view(), name = "company with name"),
-    path('', index),
-    path('perks/', PerksView.as_view()),
-    path('postjob/', PostJob),
-    path('spoc/', SpocView.as_view()),
-    path('company/', CompanyView.as_view())
+    re_path(r'^companies/?$', CompanyList.as_view(), name = "company"),
+    re_path(r'^companies/<str:name>/?$', CompanyWithName.as_view(), name = "company with name"),
+    re_path(r'^health/?$', health_response),
+    re_path(r'^perks/?$', PerksView.as_view()),
+    re_path(r'^postjob/?$', post_job),
+    re_path(r'^spoc/?$', SpocView.as_view()),
+    re_path(r'^company/?$', CompanyView.as_view()),
+    re_path(r'^profile/?$', CreateProfile.as_view()),
 ]
