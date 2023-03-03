6c8a5006478e9acabbe3abadaa8bb93650dff7d0
Achuthan S <AchuthanSivakumar.S@mrcooper.com>
2023-03-03 19:16:20 +0530

    settings update

--- naan_muthalvan/naan_muthalvan/settings.py
+++ naan_muthalvan/naan_muthalvan/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8*hwh#m%hwk^12e_096019*d!yr9q4^*7g1pve=y@m$d@93jg7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
+DEBUG = False
+LOCAL = False
+ALLOWED_HOSTS = [""]
 
 # Application definition
 
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
