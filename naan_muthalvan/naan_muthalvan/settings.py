bb647fe50e2fdff34e01e3a379e3ebc8f5ee1ef0
Achuthan S <AchuthanSivakumar.S@mrcooper.com>
2023-03-03 19:26:48 +0530

    settings update

--- naan_muthalvan/naan_muthalvan/settings.py
+++ naan_muthalvan/naan_muthalvan/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8*hwh#m%hwk^12e_096019*d!yr9q4^*7g1pve=y@m$d@93jg7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-LOCAL = False
+DEBUG = True
 ALLOWED_HOSTS = [""]
 
 # Application definition
@@ -122,9 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
